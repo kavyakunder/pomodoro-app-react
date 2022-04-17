bbfe7293c00766cf35d8dff86607f3b7544e83c8
kavya_kunder <kavyakunder7082@gmail.com>
2022-04-18 01:02:24 +0530

    refactor: shorten the taskCard info

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -14,10 +14,10 @@ function TaskCard({ id, name, description, timer, breakTimer, deleteTodo }) {
         }}
       >
         <div className="note">
-          <h1> This is the title- {name}</h1>
-          <p>This is description- {description}</p>
-          <p>This is work timer- {timer}</p>
-          <p>This is break timer- {breakTimer}</p>
+          <h1> Title is {name}</h1>
+          <p>Description is {description}</p>
+          <p> Work timer is {timer}</p>
+          <p> Break timer is {breakTimer}</p>
           <button className="btn-add" onClick={() => deleteTodo(id)}>
             Delete
           </button>
